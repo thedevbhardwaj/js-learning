17c088b6c163e23bad058eb36253aa0d4f0bd5ec
thedevbhardwaj <2021kuec2015@iiitkota.ac.in>
2024-05-24 11:26:56 +0530

    01_basics updated

---
+++ 01_basics/04_Comparison.js
@@ -0,0 +1,21 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === compares the value as well as the datatype
+
+console.log("2" === 2);
