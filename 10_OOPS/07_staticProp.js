4cde51d51bcc98f50ac821286f8364d65c63e258
thedevbhardwaj <2021kuec2015@iiitkota.ac.in>
2024-05-28 14:20:01 +0530

    static properties of class

---
+++ 10_OOPS/07_staticProp.js
@@ -0,0 +1,27 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const hitesh = new User("hitesh")
+// console.log(hitesh.createId())//Will give us a error
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+iphone.logMe();
+// console.log(iphone.createId());//give us a error
